53f36c5501b5bbac1f896df78c953b6c25218d14
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-18 20:45:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Add your doToElementsInArray() function here:
-
-
+//.forEach() method allows us to iterate over every element of the array without writing out a forLoop
+ function doToElementsInArray(array, callback){
+   array.forEach(callback)
+ }
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
